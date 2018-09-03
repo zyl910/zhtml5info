d11902b425602cdcfeb3ffeae16171c7417e52a4
zyl910 <zyl910@sina.com>
2018-09-03 16:33:51 +0800

    zyllibjs.js: add zyl.Common.exstr .

--- src/js/zyllibjs.js
+++ src/js/zyllibjs.js
@@ -43,6 +43,95 @@ zyl.Common = function () {
 			Child.uber = Parent.prototype;
 		},
 		
+		/** Get class name string (取得类名字符串).
+		 * 
+		 *  @param	{Object}	A object (一个对象).
+		 *  @return	{String}	Class name string (类名字符串). 若不是对象则返回 `undefined` .
+		 *	@static
+		 */
+		getClassName: function(obj) {
+			if (obj && obj.constructor && obj.constructor.toString()) {
+				/*
+				* for browsers which have name property in the constructor
+				* of the object,such as chrome 
+				*/
+				if(obj.constructor.name) {
+					return obj.constructor.name;
+				}
+				var str = obj.constructor.toString();
+				/*
+				* executed if the return of object.constructor.toString() is 
+				* "[object objectClass]"
+				*/
+				var arr = null;
+				if(str.charAt(0) == '[')
+				{
+					arr = str.match(/\[\w+\s*(\w+)\]/);
+				} else {
+					/*
+					* executed if the return of object.constructor.toString() is 
+					* "function objectClass () {}"
+					* for IE Firefox
+					*/
+					arr = str.match(/function\s*(\w+)/);
+				}
+				if (arr && arr.length == 2) {
+					return arr[1];
+				}
+			}
+			return undefined; 
+		},
+		
+		/** Get exception string (取得异常字符串). 即取得 `ClassType(name, code/number): message/description` 格式的字符串.
+		 * 
+		 *  @param	{Error}	e	Error object (错误对象).
+		 *  @return	{String}	Exception string (异常字符串). 若不是对象则返回 `undefined` .
+		 *	@static
+		 */
+		exstr: function(e) {
+			var rt = undefined;
+			if (typeof(e) != "object") return rt;
+			if (null==e) return rt;
+			var sclass = zyl.Common.getClassName(e);
+			var sname = e["name"];
+			var ncode = e["code"] || e["number"];
+			var smessage = e["message"] || e["description"];
+			if (!!sname) {
+				if (sname==sclass) sname='';
+			}
+			if (!smessage) {
+				smessage = e.toString();
+				if (!!smessage) {
+					if (0==smessage.indexOf("[object")) {
+						smessage = "";
+					} else {
+						if (0==smessage.indexOf(sname)) {
+							smessage = smessage.substr(sname.length + 1 + 1);
+						}
+					}
+				}
+			}
+			if (!smessage) {
+				smessage = "Unknown";
+			}
+			// make.
+			rt = sclass;
+			if (!sname) {
+				if (!ncode) {
+				} else {
+					rt += "(" + ncode + ")";
+				}
+			} else {
+				if (!ncode) {
+					rt += "(" + sname + ")";
+				} else {
+					rt += "(" + sname + ", " + ncode + ")";
+				}
+			}
+			rt += ": " + smessage;
+			return rt;
+		},
+		
 		/** Version (版本号). @static @readonly */
 		version: 0x100
 	};
