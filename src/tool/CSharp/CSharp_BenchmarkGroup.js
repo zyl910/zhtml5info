ffbbd79ebbe36a5f36d3ba62d959cc9881c8f77e
zhouyuelin <zyl910@sina.com>
2023-07-17 15:12:33 +0800

    CSharp_BenchmarkGroup.htm: ParseStatus.CODE_BEGIN .

--- src/tool/CSharp/CSharp_BenchmarkGroup.js
+++ src/tool/CSharp/CSharp_BenchmarkGroup.js
@@ -9,6 +9,73 @@ var ExportFormat = {
 };
 
 
+/** @class BenchmarkDataRecord
+ * Benchmark data - Record (基准测试信息-记录).
+ */
+function BenchmarkDataRecord(config) {
+	config = config || {};
+	/** @property {String} 名称. */
+	this.name = config.name || "";
+	/** @property {String} 微秒us. */
+	this.us = config.us || "";
+	/** @property {String} 兆每秒 (Mega/second). */
+	this.mops = config.mops || "";
+	/** @property {String} 倍数. */
+	this.scale = config.scale || "";
+}
+
+/** @class BenchmarkDataClass
+ * Benchmark data - Class (基准测试信息-类).
+ */
+function BenchmarkDataClass(config) {
+	config = config || {};
+	/** @property {String} 标题. */
+	this.title = config.title || "";
+	/** @property {String} 完整标题. */
+	this.titleFull = config.titleFull || "";
+	/** @property {BenchmarkDataRecord[]} 记录列表. */
+	this.list = config.list || [];
+}
+
+/** @class BenchmarkDataDotNet
+ * Benchmark data - DOTNET Version (基准测试信息-DOTNET 版本).
+ */
+function BenchmarkDataDotNet(config) {
+	config = config || {};
+	/** @property {String} 标题. */
+	this.title = config.title || "";
+	/** @property {BenchmarkDataClass[]} 类列表. */
+	this.list = config.list || [];
+}
+
+/** @class BenchmarkDataPlatform
+ * Benchmark data - Platform (基准测试信息-平台).
+ */
+function BenchmarkDataPlatform(config) {
+	config = config || {};
+	/** @property {String} 标题. */
+	this.title = config.title || "";
+	/** @property {String} 2级标题. */
+	this.titleLv2 = config.titleLv2 || "";
+	/** @property {String} 3级标题. */
+	this.titleLv3 = config.titleLv3 || "";
+	/** @property {String} 4级标题. */
+	this.titleLv4 = config.titleLv4 || "";
+	/** @property {BenchmarkDataDotNet[]} DOTNET 版本列表. */
+	this.list = config.list || [];
+}
+
+/** @class BenchmarkData
+ * Benchmark data (基准测试信息).
+ */
+function BenchmarkData(config) {
+	config = config || {};
+	/** @property {String} 标题. */
+	this.title = config.title || "";
+	/** @property {BenchmarkDataPlatform[]} 平台列表. */
+	this.list = config.list || [];
+}
+
 const clipboardObj = navigator.clipboard;
 var m_exportFormat = ExportFormat.FORMAT_TAB;
 
@@ -75,6 +142,29 @@ function pasteAndRun(callback) {
 	}
 }
 
+/** Make title..
+ *
+ * @param {String[]}	titleArray	Title array.
+ * @param {Number}	levelBegin	Level begin.
+ * @param {Number}	levelEnd	Level end.
+ * @return {String}	Return title.
+ */
+function makeTitle(titleArray, levelBegin, levelEnd) {
+	var title = "";
+	var i;
+	var src;
+	if (levelBegin<1) levelBegin=1;
+	if (levelEnd>titleArray.length) levelEnd=titleArray.length;
+	for(i=levelBegin; i<=levelEnd; ++i) {
+		src = titleArray[i-1];
+		if (!title) {
+			title = src;
+		} else {
+			title += " - " + src;
+		}
+	}
+	return title;
+}
 
 /** Benchmark - Parse.
  *
@@ -82,7 +172,96 @@ function pasteAndRun(callback) {
  * @return {BenchmarkData}	Return benchmark data.
  */
 function benchmarkParse(lines) {
-	return {test:"TEST!"};
+	var SYMBOL_CODE_AREA = "```";
+	var POUND_SIGN = '#';
+	var ParseStatus = {
+		INIT: 0,
+		TITLE: 1,
+		CODE_END: 2,
+		CODE_BEGIN: 3,
+		CLASS: 4,
+		RECORD: 5
+	};
+	var parseStatus = ParseStatus.INIT;
+	var titleArray = []; // Item `x` is title `x+1`.
+	var titleLevel = 0;
+	var benchmarkData = new BenchmarkData();
+	var dataPlatform = null;
+	var dataDotNet = null;
+	var dataClass = null;
+	var dataRecord = null;
+	var line, src;
+	var n, newLevel;
+	var i, j;
+	for(i=0; i<lines.length; ++i) {
+		line = lines[i];
+		if (""==line || line.length<=0) continue;
+		line = line.trim();
+		if (""==line || line.length<=0) continue;
+		n = line.indexOf(SYMBOL_CODE_AREA);
+		if (0==n) {
+			if (ParseStatus.CODE_BEGIN > parseStatus) {
+				parseStatus = ParseStatus.CODE_BEGIN;
+				dataClass = null;
+				if (null==dataPlatform) {
+					dataPlatform = new BenchmarkDataPlatform();
+					if (titleLevel>=2) {
+						dataPlatform.title = makeTitle(titleArray, 2, titleLevel-1);
+						dataPlatform.titleLv2 = titleArray[2-1];
+					}
+					if (titleLevel>=3) {
+						dataPlatform.titleLv3 = titleArray[3-1];
+					}
+					if (titleLevel>=4) {
+						dataPlatform.titleLv4 = titleArray[4-1];
+					}
+					benchmarkData.list.push(dataPlatform);
+				}
+				if (null==dataDotNet) {
+					dataDotNet = new BenchmarkDataDotNet();
+					if (titleLevel>0) {
+						dataDotNet.title = titleArray[titleLevel-1];
+					}
+					dataPlatform.list.push(dataDotNet);
+				}
+			} else {
+				parseStatus = ParseStatus.CODE_END;
+			}
+		} else {
+			if (ParseStatus.CODE_BEGIN > parseStatus) {
+				if (line.charAt(0) == POUND_SIGN) {
+					parseStatus = ParseStatus.TITLE;
+					newLevel = 0;
+					for(j=0; j<line.length; ++j) {
+						if (line.charAt(j) == POUND_SIGN) {
+							++newLevel;
+							if (titleArray.length<newLevel) {
+								titleArray.push("");
+							}
+						}
+					}
+					src = line.substring(newLevel).trim();
+					titleLevel = newLevel;
+					titleArray[titleLevel-1] = src;
+					if (1==titleLevel && src.length>0) {
+						if (!benchmarkData.title) {
+							benchmarkData.title = src;
+						}
+					}
+					if (null!=dataDotNet) {
+						dataDotNet = null;
+					}
+					if (null!=dataPlatform && titleLevel<=3) {
+						dataPlatform = null;
+					}
+				}
+			} else if (ParseStatus.CODE_BEGIN == parseStatus) {
+			} else if (ParseStatus.CLASS == parseStatus) {
+			} else if (ParseStatus.RECORD == parseStatus) {
+			}
+		}
+	}
+	return benchmarkData;
 }
 
 /** Benchmark - Group.
